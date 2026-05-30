946725383e0e8c71822505b37ce86dd521778cfb
c2587871235-star <c2587871235@gmail.com>
2026-05-31 06:52:56 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'shijian-v5';
+const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(()=>{}))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).then(r => { if(r&&r.status===200){const c=r.clone();caches.open(CACHE_NAME).then(ca=>ca.put(e.request,c));} return r; }).catch(()=>caches.match(e.request))); });
